238d56b3c51dd58bad3f63c97da4611f03407297
Santiago Romero <sontr@MacBook-Air-de-Santiago.local>
2024-07-09 14:31:57 -0300

    chore: Add API key authentication middleware

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const routerApi = require('./routes');
+const {checkApiKey} = require('./middlewares/auth.handler')
+
 require('dotenv').config()
 
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
-const port = process.env.PORT || 3000;
 
-app.use(express.json());
+
+
 
 const whitelist = ['http://localhost:8080', 'https://myapp.co'];
 const options = {
@@ -20,16 +22,28 @@ const options = {
     }
   }
 }
+
+
+//ruta de prueba 
+app.get('/nueva-ruta',checkApiKey,(req,res)=>{
+  res.send('hola este es mi server')
+})
+
+
+
+
+//middlewares
 app.use(cors(options));
-
-
-routerApi(app);
-
+app.use(express.json());
 app.use(logErrors);
 app.use(boomErrorHandler);
 app.use(errorHandler);
 
 
+
+//routes/server
+routerApi(app);
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log('Mi port' +  port);
 });
