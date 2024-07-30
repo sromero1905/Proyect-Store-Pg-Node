8a668813e61d4b0d2f9f35dcf34a959e418f106b
Santiago Romero <santirom1905@gmail.com>
2024-07-30 20:06:06 -0300

    se arregla error de routes

--- routes/profile.router.js
+++ routes/profile.router.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const passport = require('passport');
-const OrderService = require('../services/order.service');
 
+const OrderService = require('../services/order.service');
 
 const router = express.Router();
 const service = new OrderService();
@@ -9,14 +9,14 @@ const service = new OrderService();
 router.get('/my-orders',
   passport.authenticate('jwt', {session: false}),
   async (req, res, next) => {
-  try {
-    const user = req.user
-    const orders = await service.findByUser(user.sub);
-    res.json()
-  } catch (error) {
-    next(error);
+    try {
+      const user = req.user;
+      const orders = await service.findByUser(user.sub);
+      res.json(orders);
+    } catch (error) {
+      next(error);
+    }
   }
-});
-
+);
 
 module.exports = router;
